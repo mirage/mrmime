4dcd24888db505e8654c9692faf2ed8031c04a0f
Calascibetta Romain <romain.calascibetta@gmail.com>
2018-12-07 18:20:04 +0100

    Add value function on Option module

--- lib/option.ml
+++ lib/option.ml
@@ -2,3 +2,7 @@ let bind f = function Some x -> f x | None -> None
 let map f = function Some x -> Some (f x) | None -> None
 let ( >>= ) x f = bind f x
 let ( >>| ) x f = map f x
+
+let value ~default = function
+  | Some x -> x
+  | None -> default
