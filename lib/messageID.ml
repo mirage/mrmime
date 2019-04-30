c57dc5dc0eff0dccb4efc27a0913cb01b6c6c3b7
Calascibetta Romain <romain.calascibetta@gmail.com>
2019-04-30 19:36:46 +0200

    Add a proper way to make a MessageID domain (same as Mailbox)

--- lib/messageID.ml
+++ lib/messageID.ml
@@ -59,3 +59,107 @@ module Encoder = struct
 end
 
 let to_unstructured ~field_name x = Unstructured.to_unstructured ~field_name Encoder.message_id x
+
+let is_utf8_valid_string_with is x =
+  let exception Invalid_utf8 in
+  let exception Invalid_char in
+  try
+    Uutf.String.fold_utf_8
+      (fun () _pos -> function `Malformed _ -> raise Invalid_utf8
+        | `Uchar uchar ->
+            if Uchar.is_char uchar && not (is (Uchar.to_char uchar)) then
+              raise Invalid_char )
+      () x ;
+    true
+  with
+  | Invalid_utf8 -> false
+  | Invalid_char -> false
+
+let is_utf8_valid_string x =
+  let exception Invalid_utf8 in
+  try
+    Uutf.String.fold_utf_8
+      (fun () _pos -> function `Malformed _ -> raise Invalid_utf8 | _ -> ())
+      () x ;
+    true
+  with Invalid_utf8 -> false
+
+let is_atext_valid_string = is_utf8_valid_string_with Rfc822.is_atext
+let is_dtext_valid_string = is_utf8_valid_string_with Rfc822.is_dtext
+let is_qtext_valid_string = is_utf8_valid_string_with Rfc822.is_qtext
+
+module Domain = struct
+  let atom x = if is_atext_valid_string x then Some (`Atom x) else None
+
+  let atom_exn x =
+    match atom x with
+    | Some v -> v
+    | None -> Fmt.invalid_arg "atom_exn: invalid atom value %S" x
+
+  let a = atom_exn
+
+  let literal x =
+    let need_to_escape, escape_char =
+      (* TODO *)
+      let bindings = [('\000', '\000')] in
+      ( (fun chr -> List.mem_assoc chr bindings)
+      , fun chr -> List.assoc chr bindings )
+    in
+    let escape_string x =
+      let len = String.length x in
+      let res = Buffer.create (len * 2) in
+      let pos = ref 0 in
+      while !pos < len do
+        if need_to_escape x.[!pos] then (
+          Buffer.add_char res '\\' ;
+          Buffer.add_char res (escape_char x.[!pos]) )
+        else Buffer.add_char res x.[!pos] ;
+        incr pos
+      done ;
+      Buffer.contents res
+    in
+    if is_dtext_valid_string x then Some (`Literal x)
+    else if is_utf8_valid_string x then Some (`Literal (escape_string x))
+    else None
+
+  let literal_exn x =
+    match literal x with
+    | Some v -> v
+    | None -> Fmt.invalid_arg "literal_exn: invalid domain literal value %S" x
+
+  type atom = [`Atom of string]
+  type literal = [`Literal of string]
+
+  type 'a domain =
+    | ( :: ) : atom * 'a domain -> 'a Peano.s domain
+    | [] : Peano.z domain
+
+  let rec coerce : type a. a Peano.s domain -> string list = function
+    | [`Atom x] -> [x]
+    | `Atom x :: y :: r -> List.cons x (coerce (y :: r))
+
+  let make_domain : type a. a domain -> string list option = function
+    | [] -> None
+    | x :: r -> Some (coerce (x :: r))
+
+  type 'a t =
+    | Domain : 'a domain t
+    | Literal : string t
+
+  let domain = Domain
+  let default = Literal
+
+  let make : type a. a t -> a -> Rfc822.nonsense Rfc822.domain option =
+   fun witness v ->
+    match witness with
+    | Domain -> Option.(make_domain v >>| fun v -> `Domain v)
+    | Literal -> literal v
+
+  let v : type a. a t -> a -> Rfc822.nonsense Rfc822.domain =
+   fun witness v ->
+    match make witness v with
+    | Some v -> v
+    | None -> Fmt.invalid_arg "make_exn: invalid domain"
+
+  let to_string x = Encoder.to_string Encoder.domain x
+end
--- lib/messageID.mli
+++ lib/messageID.mli
@@ -3,6 +3,82 @@ type domain = Rfc822.nonsense Rfc822.domain
 type local = Rfc822.local
 type t = Rfc822.nonsense Rfc822.msg_id
 
+module Domain : sig
+  (** A RFC 822 domain can be constructed in two way.
+      This construction {b differ} from {!Address.domain}.
+      We are allow to construct a common domain like:
+
+      {[
+        let isomorphism = Domain.(v domain [ a "isomorphis"; a "me" ]) ;;
+        val isomorphism : domain = `Domain ["isomorphis"; "me"]
+        Domain.to_string isomorphism ;;
+        - : string = "isomorphis.me"
+      ]}
+
+      Or a {i literal} domain:
+
+      {[
+        let x25519 = Domain.(v literal "x25519") ;;
+        val x25519 : domain = `Literal "x25519"
+        Domain.to_string x25519 ;;
+        - : string = "[x25519]"
+      ]} *)
+
+  type atom = [`Atom of string]
+  type literal = [`Literal of string]
+
+  type 'a domain =
+    | ( :: ) : atom * 'a domain -> 'a Peano.s domain
+    | [] : Peano.z domain
+
+  type 'a t
+  (** Kind of domain according RFC 822.
+
+      {ul
+      {- An usual {!domain} which is a non-empty list of {!atom} elements}
+      {- A [`Literal] domain which is a string surrounded by brackets.}} *)
+
+  val atom : string -> atom option
+  (** [atom x] returns a safe {!atom} element. If [x] does not respect RFC 5322,
+     it returns [None]. It accepts any characters excepts controls, space and
+     specials characters - for instance, brackets are not allowed. *)
+
+  val atom_exn : string -> atom
+  (** Same as {!atom} but raises an [Invalid_argument] instead [None]. *)
+
+  val a : string -> atom
+  (** Alias of {!atom_exn}. *)
+
+  val literal : string -> literal option
+  (** [literal x] returns a {!literal} domain. If [x] does not respect RFC 5321,
+     it returns [None]. It will try to escape control characters
+     (with {!escape_string}). *)
+
+  val literal_exn : string -> literal
+  (** Same as {!literal} but raises an [Invalid_argument] instead to return [None]. *)
+
+  val domain : 'a domain t
+  (** Kind of domain. *)
+
+  val default : string t
+  (** Kind of {!literal}. *)
+
+  val make : 'a t -> 'a -> Rfc822.nonsense Rfc822.domain option
+  (** [make kind v] returns a safe domain. It can fail if an user-defined
+     literal-domain ({!Literal_domain.extension}), a {!literal} domain or a
+     {!domain} don't follow standards:
+
+     {ul
+     {- for a {!literal}, [make] returns [None] if {!literal} returns [None]}
+     {- for a {!domain}, [make] returns [None] if list of {!atom} is empty}} *)
+
+  val v : 'a t -> 'a -> Rfc822.nonsense Rfc822.domain
+  (** Same as {!make} but raises an [Invalid_argument] instead [None]. *)
+
+  val to_string : Rfc822.nonsense Rfc822.domain -> string
+  (** [to_string x] returns a string which represents [x] as is it in a e-mails. *)
+end
+
 (** {2 Pretty-printers.} *)
 
 val pp_word : word Fmt.t
