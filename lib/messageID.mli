784631e65f86d21d0856142660f57cd1d8074221
Anil Madhavapeddy <anil@recoil.org>
2019-04-30 19:57:34 +0200

    Update lib/messageID.mli
    Co-Authored-By: dinosaure <romain.calascibetta@gmail.com>

--- lib/messageID.mli
+++ lib/messageID.mli
@@ -5,7 +5,7 @@ type t = Rfc822.nonsense Rfc822.msg_id
 
 module Domain : sig
   (** An RFC 822 domain can be constructed in two ways.
-      This construction {b differ} from {!Address.domain}.
+      This construction {b differs} from {!Address.domain}.
       We are allow to construct a common domain like:
 
       {[
