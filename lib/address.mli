8b7b13d3b1d1553bd3b950f5c814a61353fc0548
Calascibetta Romain <romain.calascibetta@gmail.com>
2019-04-30 19:11:00 +0200

    Add documentation on Address module

--- lib/address.mli
+++ lib/address.mli
@@ -1,9 +1,19 @@
 type t = [`Group of Group.t | `Mailbox of Mailbox.t]
+(** Type of address, an address may either be an individual mailbox, or a group
+   of mailboxes. *)
 
 val group : Group.t -> t
+(** [group g] returns an address from a {!Group.t}. *)
+
 val mailbox : Mailbox.t -> t
+(** [mailbox m] returns an address from a {!Mailbox.t}. *)
+
+(** {2 Pretty-printers.} *)
 
 val pp : t Fmt.t
+(** Pretty-printer of {!t}. *)
+
+(** {2 Encoder of address.} *)
 
 module Encoder : sig
   val address : t Encoder.encoding
