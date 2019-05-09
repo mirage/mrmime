5523b83da3a29f6f76f39120a202d4c3f7e29bad
Calascibetta Romain <romain.calascibetta@gmail.com>
2019-05-09 23:51:07 +0200

    Add *.with_emitter functions in Rfc5322/Quoted_printable/B64 modules

--- lib/b64.ml
+++ lib/b64.ml
@@ -63,6 +63,10 @@ let with_buffer end_of_body =
 
   parser ~write_data end_of_body >>| fun () -> Buffer.contents buf
 
+let with_emitter ~emitter end_of_body =
+  let write_data x = emitter (Some x) in
+  parser ~write_data end_of_body
+
 let to_end_of_input ~write_data =
   let dec = Base64_rfc2045.decoder `Manual in
 
