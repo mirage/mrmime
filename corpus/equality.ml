2f9f715274d36c60117e6a6c30d61048773ffafa
Carine Morel <carine@tarides.com>
2021-06-17 18:35:55 +0200

    Comparison functions for headers.

--- corpus/equality.ml
+++ corpus/equality.ml
@@ -2,6 +2,46 @@ open Mrmime
 
 exception NotEqual of string
 
+let all_field_names =
+  Field_name.
+    [
+      date; from; sender; reply_to; cc; bcc; subject; message_id; in_reply_to;
+      references; comments; keywords; received; return_path; content_type;
+      content_encoding; mime_version; content_id; content_description;
+      resent_date; resent_from; resent_sender; resent_to; resent_cc; resent_bcc;
+      resent_message_id; resent_reply_to;
+    ]
+
+let rec compare_sorted_list ((only_in_1, only_in_2) as acc) h1 h2 =
+  match (h1, h2) with
+  | [], [] -> acc
+  | xs, [] -> (xs @ only_in_1, only_in_2)
+  | [], xs -> (only_in_1, xs @ only_in_2)
+  | x :: xs, y :: ys ->
+      if x = y then compare_sorted_list acc xs ys
+      else if compare x y < 0 then
+        compare_sorted_list (x :: only_in_1, only_in_2) xs h2
+      else compare_sorted_list (only_in_1, y :: only_in_2) h1 ys
+
+let sort = List.sort_uniq Stdlib.compare
+
+let compare_header (h : Header.t) (h' : Header.t) =
+  let rec go h h' =
+    let all1 =
+      List.map (fun name -> (name, Header.assoc name h)) all_field_names
+    in
+    let all2 = List.map (fun name -> Header.assoc name h') all_field_names in
+    List.fold_left2
+      (fun (h1, h2) (n, l1) l2 ->
+        let only_in_1, only_in_2 =
+          compare_sorted_list ([], []) (sort l1) (sort l2)
+        in
+        ((n, only_in_1) :: h1, (n, only_in_2) :: h2))
+      ([], []) all1 all2
+  in
+  go h h'
+
+
 let same_structure m1 m2 =
   let rec go m1 m2 =
     match (m1, m2) with
